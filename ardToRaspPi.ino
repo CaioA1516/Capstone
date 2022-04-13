4c654ed01b02a2793087c2945b5ebb91a487fe33
Mr-Ostrich <60199485+Mr-Ostrich@users.noreply.github.com>
2022-04-13 11:44:43 -0400

    Updated to fit 5 buttons
    5 buttons instead of 4 now

--- ardToRaspPi.ino
+++ ardToRaspPi.ino
@@ -6,6 +6,7 @@
 #define BUTTON_LEFT     A4
 #define BUTTON_DOWN     A3
 #define BUTTON_RIGHT    A2
+#define BUTTON_PAUSE    A1
 
 /* Define the Bytes we need to send for a press of each key. Edit here
   to change the keys pressed for whatever game you want to play. For a list
@@ -15,27 +16,27 @@
 #define KEY_PRESS_W         0x77  // correct
 #define KEY_PRESS_A         0x61  // correct
 #define KEY_PRESS_S         0x73  // correct    
-#define KEY_PRESS_D         0x64  // correct             
+#define KEY_PRESS_D         0x64  // correct
+#define KEY_PRESS_Q         0x71  // correct             
 
 // This section takes the key presses defined above and maps them to the buttons on the controller
-uint8_t buttons[4][2] = {{BUTTON_UP, KEY_PRESS_W}, // Participants must choose what key will be mapped to each button here, for now, this will be D
+uint8_t buttons[5][2] = {{BUTTON_UP, KEY_PRESS_W}, // Participants must choose what key will be mapped to each button here, for now, this will be D
   {BUTTON_LEFT, KEY_PRESS_A}, // Participants must choose what key will be mapped to each button here, for now, this will be S
   {BUTTON_DOWN, KEY_PRESS_S}, // Participants must choose what key will be mapped to each button here, for now, this will be W
-  {BUTTON_RIGHT, KEY_PRESS_D} // Participants must choose what key will be mapped to each button here, for now, this will be A
+  {BUTTON_RIGHT, KEY_PRESS_D},// Participants must choose what key will be mapped to each button here, for now, this will be A
+  {BUTTON_PAUSE, KEY_PRESS_Q} // Participants must choose what key will be mapped to each button here, for now, this will be Q
 };
 
-// Button contact bounce compensation
-const int DEBOUNCE = 100;
 // Communication latency compensation
 const int LATENCY  = 75;
-int threshold = 450;
-int whichButtonPressed[4] = {0, 0, 0, 0};
+int threshold = 400;
+int whichButtonPressed[5] = {0, 0, 0, 0, 0};
 int val; // temporary value
-bool prevButtonPressed[4] = {false, false, false, false};
+bool prevButtonPressed[5] = {false, false, false, false, false};
 
 void setup() {
   // Setup all buttons as input pins
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 5; i++) {
     pinMode(buttons[i][0], INPUT_PULLUP);
   }
 
@@ -46,7 +47,7 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < 4; i++ ) {
+  for (int i = 0; i < 5; i++ ) {
     val = analogRead(buttons[i][0]);
     whichButtonPressed[i] = val;
     Serial.print(val);
@@ -58,7 +59,7 @@ void loop() {
   Serial.println("");
 
 
-  for (int i = 0; i < 4; i++ ) {
+  for (int i = 0; i < 5; i++ ) {
     if (whichButtonPressed[i] > threshold && prevButtonPressed[i] == false) {
       Keyboard.write(buttons[i][1]);
       prevButtonPressed[i] = true;
@@ -67,11 +68,5 @@ void loop() {
     }
   }
 
-  /*
-  Serial.print(prevButtonPressed[0]);
-  Serial.print(prevButtonPressed[1]);
-  Serial.print(prevButtonPressed[2]);
-  Serial.println(prevButtonPressed[3]);
-  */
   delay(LATENCY);
 }
