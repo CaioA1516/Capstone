20a15315ded705c30a1c5016d5296cac286b0cb4
CaioA1516 <56455433+CaioA1516@users.noreply.github.com>
2022-05-06 08:30:50 -0400

    Uploaded updated Arduino code

---
+++ mat_input.ino
@@ -0,0 +1,89 @@
+#include "Keyboard.h"
+
+/* Arduino to RaspPi Code */
+//Define the Buttons we are using in the hardware
+#define BUTTON_UP       A5
+#define BUTTON_LEFT     A1
+#define BUTTON_DOWN     A2
+#define BUTTON_RIGHT    A4
+#define BUTTON_PAUSE    A3
+#define CALIBRATION_PIN 7
+/* Define the Bytes we need to send for a press of each key. Edit here
+  to change the keys pressed for whatever game you want to play. For a list
+  of keyboard modifier hex values, check out:
+  http://arduino.cc/en/Reference/KeyboardModifiers  */
+
+#define KEY_PRESS_W         0x77  // correct
+#define KEY_PRESS_A         0x61  // correct
+#define KEY_PRESS_S         0x73  // correct    
+#define KEY_PRESS_D         0x64  // correct
+#define KEY_PRESS_Q         0x71  // correct             
+
+// This section takes the key presses defined above and maps them to the buttons on the controller
+uint8_t buttons[5][2] = {{BUTTON_UP, KEY_PRESS_W}, // Participants must choose what key will be mapped to each button here, for now, this will be D
+  {BUTTON_LEFT, KEY_PRESS_A}, // Participants must choose what key will be mapped to each button here, for now, this will be S
+  {BUTTON_DOWN, KEY_PRESS_S}, // Participants must choose what key will be mapped to each button here, for now, this will be W
+  {BUTTON_RIGHT, KEY_PRESS_D},// Participants must choose what key will be mapped to each button here, for now, this will be A
+  {BUTTON_PAUSE, KEY_PRESS_Q} // Participants must choose what key will be mapped to each button here, for now, this will be Q
+};
+
+// Communication latency compensation
+const int LATENCY  = 50;
+int threshold[5] = {125, 125, 125, 125, 125}; // w, a, s, d, q
+int whichButtonPressed[5] = {0, 0, 0, 0, 0};
+int val; // temporary value
+bool prevButtonPressed[5] = {false, false, false, false, false};
+
+void calibrate() {
+  for (int i = 0; i < 5; i++ ) {
+    val = analogRead(buttons[i][0]);
+    threshold[i] = val + 30;
+  }
+}
+
+void setup() {
+  // Setup all buttons as input pins
+  for (int i = 0; i < 5; i++) {
+    pinMode(buttons[i][0], INPUT_PULLUP);
+  }
+  // Setup calibration pin
+  pinMode(CALIBRATION_PIN, INPUT);
+  // Begin the HID protocol for a keyboard
+  Keyboard.begin();
+  Serial.begin(115200);
+  delay(1000);
+  calibrate();
+
+}
+
+void loop() {
+  Serial.println(digitalRead(CALIBRATION_PIN));
+  if (digitalRead(CALIBRATION_PIN) == true){
+    Serial.println("Calibrating!");
+    calibrate();
+  }
+  for (int i = 0; i < 5; i++ ) {
+    val = analogRead(buttons[i][0]);
+    whichButtonPressed[i] = val;
+    Serial.print(val);
+    Serial.print(" - ");
+    Serial.print(threshold[i]);
+    Serial.print(", ");
+    if (val > threshold[i]) {
+      delayMicroseconds(100);
+    }
+  }
+  Serial.println("");
+
+
+  for (int i = 0; i < 5; i++ ) {
+    if (whichButtonPressed[i] > threshold[i] && prevButtonPressed[i] == false) {
+      Keyboard.write(buttons[i][1]);
+      prevButtonPressed[i] = true;
+    } else if (whichButtonPressed[i] <= threshold[i] && prevButtonPressed[i] == true) {
+      prevButtonPressed[i] = false;
+    }
+  }
+
+  delay(LATENCY);
+}
